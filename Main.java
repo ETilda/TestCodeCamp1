e999d01d1ce16757cbe9702f1ec640003d8818fa
ETilda <ejiro.orajekwe@gmail.com>
2021-10-05 21:12:30 +0100

    BasisJavaProject13

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World. This is my first project");
+        System.out.println("Hurray!!! I'm here finally!");
+        System.out.println("It's going to be a smooth and sweet ride henceforth! ");
+        System.out.println(" Come ride with me! It promises to be fun.");
+        System.out.println("I believe you are enjoying every bit of this.");
+        System.out.println("Play around and gain understanding of Java.");
+    }
+    }
