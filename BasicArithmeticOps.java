7e90c3136a432588aac4613d3103a1ec725084d4
ETilda <ejiro.orajekwe@gmail.com>
2021-10-05 21:47:21 +0100

    BasicArithmeticOPTask16

---
+++ BasicArithmeticOps.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class BasicArithmeticOps {
+    public static void main(String[] args) {
+        //basic arithmetic
+        //double age1 = 20;
+        //double age2 = 30;
+        //System.out.println(age1);
+        //incremental and decremental ++ --
+        //age1--;
+
+        //compound assignment operators += -= /= *=
+        //age1 *= age2;
+        //20 * 30 =600;
+        //age1 = 600;
+        //System.out.println(age1);
+
+
+//Task 16: Find the area of a circle with radius on 12.7 m. Print out your result with the unit of measurement which is metres
+        double r = 12.7;
+        double pie = 3.142;
+        double area = (pie * r * r);
+        System.out.println(area + "m");
+
+    }
+}
