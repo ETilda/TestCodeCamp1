9684100760bbcb93cfbbb1cf3be3e6c436aa9328
ETilda <ejiro.orajekwe@gmail.com>
2021-10-05 22:39:49 +0100

    JavaLoopsOddEven Task21A

---
+++ JavaLoops.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class JavaLoops {
+    public static void main(String[] args) {
+
+        //Task 21A Odd/Even
+
+
+        int number = 10;
+
+        for ( int i = 0; i <= 10; i ++ ) {
+            if (i % 2 == 0)
+                System.out.println("odd number");
+
+            else
+                System.out.println("even number");
+
+        }
+    }
+
+    }
+
