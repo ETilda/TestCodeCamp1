a39f816f60e7b30ad7038385048ba8e17b45c8f9
ETilda <ejiro.orajekwe@gmail.com>
2021-10-05 21:21:01 +0100

    JavaDataTypes14

---
+++ JavaDataTypes.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class JavaDataTypes {
+    public static void main(String[] args) {
+//    byte from -128 to 127
+//    short from -32,768 to 32,767
+//    int from -2, i47, 438, 648, to 2, 147, 438, 648, 64
+//    long from 9, 223, 372, 083, 854, 775, 808 to 9, 223, 372, 036, 854, 775, 887
+//    float = 4 bytes = from 6 to 7 decimal digits
+//    double from 15 decimal digits
+//    char = single letters 'a' 'b'
+//    boolean = true, false
+
+
+        byte age =127;
+
+        short money = 32_766;
+
+        int soMuchMoney = 2_147_438_648;
+
+        long amountWon = 9_223_372_083_854l;
+
+        float areaCovered = 12345678.90f;
+
+        double totalCost= 185058965464.89;
+
+        char alphabet = 'A';
+
+        boolean isCold = false;
+
+
+    }
+}
